9f1f9828b7f55fc2ce53c427c7611577b1a683a1
dm1trykrylov <231014d@gmail.com>
2024-01-27 18:10:56 +0400

    Parse filename from args

--- driver.cpp
+++ driver.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 
-#include "InterpreterVisitor.h"
-#include "PrintVisitor.h"
-#include "IRVisitor.h"
-#include "antlr4-runtime.h"
 #include "ExprLexer.h"
 #include "ExprParser.h"
+#include "IRVisitor.h"
+#include "InterpreterVisitor.h"
+#include "PrintVisitor.h"
+#include "antlr4-runtime.h"
 
 int main(int argc, const char* argv[]) {
   std::ifstream stream;
-  stream.open("../input.txt");
+  std::string filename = "../input.txt";
+  if (argc == 2) {  // custom program file
+    filename = argv[1];
+  }
+  stream.open(filename);
 
   antlr4::ANTLRInputStream input(stream);
   ExprLexer lexer(&input);
@@ -21,23 +25,13 @@ int main(int argc, const char* argv[]) {
   InterpreterVisitor visitor;
   PrintVisitor print_visitor;
   IRVisitor ir_visitor;
-  // try {
-  //   print_visitor.visitFile(tree);
-  // } catch (const std::exception& ex) {
-  //   std::cout << "Error occured in print pass!\n" << ex.what() << std::endl;
-  // }
-  // try {
-  //   visitor.visitFile(tree);
-  // } catch (const std::exception& ex) {
-  //   std::cout << "Error occured in interpret pass!\n" << ex.what() << std::endl;
-  // }
   try {
     ir_visitor.visitFile(tree);
     ir_visitor.printIR();
-    ir_visitor.printIR("driver.ll");
+    ir_visitor.printIR("program.ll");
   } catch (const std::exception& ex) {
-    std::cout << "Error occured in translation pass!\n" << ex.what() << std::endl;
+    std::cout << "Error occured in translation pass!\n"
+              << ex.what() << std::endl;
   }
-
   return 0;
 }
